34ccc03b0006a96460efc908a86c6c63d08e366e
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-09-26 13:48:06 +0200

    new pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import './App.css'
-import Home from './pages/Home'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import {HashRouter as Router, Route, Routes} from 'react-router-dom'
+import "./App.css"
+import Home from "./pages/Home"
+import Catalog from "./pages/Catalog"
+import Contact from "./pages/Contact"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
- 
-
   return (
     <>
-    <Router>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
-    <Footer/>
-    </Router>
-   
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/catalog" element={<Catalog/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,19 @@
-import '../styles/Footer.css'
-import InstagramIcon from '@mui/icons-material/Instagram'
-import FacebookIcon from '@mui/icons-material/Facebook'
-import TwitterIcon from '@mui/icons-material/Twitter'
+import "../styles/Footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 function Footer() {
   return (
-    <div className='footer'>
-        <div className='socialMedia'>
-            <InstagramIcon className='icon'/>
-            <FacebookIcon className='icon'/>
-            <TwitterIcon className='icon'/>
-        </div>
-        <p>&copy; 2023 doggydaycare.com</p>
-      
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon className="icon" />
+        <FacebookIcon className="icon" />
+        <TwitterIcon className="icon" />
+      </div>
+      <p>&copy; 2023 doggydaycare.com</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
