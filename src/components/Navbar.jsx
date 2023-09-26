da32c026f91fef3b4b344a494438dbdc8c2920b0
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-09-26 11:45:29 +0200

    navbar with elements edited. (!important y/n?)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
-import Logo from '../assets/bluelonglogo.png'
+import Logo from '../assets/logo1.png'
 import '../styles/Navbar.css'
-import {Link} from 'react-router-dom'
+import {Link, useLocation} from 'react-router-dom'
 
 
 function Navbar() {
+
+  const location = useLocation()
+
   return (
     <div className='navbar'>
         <div className='logo-left'>
@@ -12,9 +15,9 @@ function Navbar() {
         </Link>
         </div>
         <div className='nav-links'>
-        <Link to='/'>Home</Link>
-            <Link to='/catalog'>Catalog</Link>
-            <Link to='/contact'>Contact</Link>
+        <Link to='/' className={location.pathname === '/' ? 'active-link' : ''}>HOME</Link>
+        <Link to='/catalog' className={location.pathname === '/catalog' ? 'active-link' : ''}>CATALOG</Link>
+        <Link to='/contact' className={location.pathname === '/contact' ? 'active-link' : ''}>CONTACT</Link>
             </div>
     </div>
   )
