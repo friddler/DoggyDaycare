506e30cb7f33efea8bb0abf424189db5edbac999
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-09-29 11:39:46 +0200

    catalog error handled, it also runs async

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,30 +1,49 @@
-import { useState, useEffect } from 'react';
-import '../styles/Catalog.css'
+import { useState, useEffect } from "react";
+import "../styles/Catalog.css";
 
 function Catalog() {
   const [dogs, setDogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f5683')
-      .then((response) => response.json())
-      .then((data) => {
-        setDogs(data.record);
-      })
-      .catch((error) => {
-        console.error('FEL FEL FEL', error);
-      });
+    fetchData();
   }, []);
 
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://api.jsonbin.io/v3/b/650a7ebece39bb6dce7f568"
+      );
+
+      if (!response.ok) {
+        throw Error("Doesn't work");
+      }
+
+      const data = await response.json();
+      setDogs(data.record);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("FEL FEL FEL", error);
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <div className='catalog'>
-      <h1 className='catalogTitle'>Our Dogs</h1>
-      <div className='catalogList'>
-        {dogs.map((dog) => (
-          <div key={dog.chipNumber} className='dog'>
-            <img src={dog.img} alt={dog.name} />
-            <h1>{dog.name}</h1>
-          </div>
-        ))}
+    <div className="catalog">
+      <h1 className="catalogTitle">Our Dogs</h1>
+      <div className="catalogList">
+        {isLoading ? (
+          <h1 className="errorText">Loading dogs...</h1>
+        ) : dogs.length === 0 ? (
+          <h1 className="errorText">No dogs found &#128546;</h1>
+        ) : (
+          dogs.map((dog) => (
+            <div key={dog.chipNumber} className="dog">
+              <img src={dog.img} alt={dog.name} />
+              <h1>{dog.name}</h1>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
