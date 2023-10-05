21ba9f1ece1ae845b3740c15aada74c972ece25c
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-10-05 11:57:27 +0200

    i can now navigate to the dog i selected

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css"
 import Home from "./pages/Home"
 import Catalog from "./pages/Catalog"
 import Contact from "./pages/Contact"
+import DogDetail from "./components/DogDetail"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import { HashRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
+
+
   return (
     <>
     <div className='app'>
@@ -16,6 +19,7 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/catalog" element={<Catalog/>} />
           <Route path="/contact" element={<Contact/>} />
+          <Route path="/catalog/:name" element={<DogDetail />} />
         </Routes>
         <Footer />
       </Router>
