05d6b24d94cb3c7a8dd7e4791fa4d4c8872f8d12
Frida <yhma22.nilsfr@folkuniversitetet.nu>
2023-10-06 09:48:37 +0200

    added materialui icons, css edited.

--- src/pages/DogDetail.jsx
+++ src/pages/DogDetail.jsx
@@ -1,5 +1,7 @@
 import "../styles/DogDetail.css";
 import { useLocation } from "react-router-dom";
+import PhoneIcon from '@mui/icons-material/PhoneIphoneRounded';
+import PersonIcon from '@mui/icons-material/Person';
 
 const DogDetail = () => {
 
@@ -13,17 +15,15 @@ const DogDetail = () => {
   return (
     <div className="dogDetail">
         <div className="dog-container">
-      <h1>{dog.name}</h1>
+      <h1>Hi! I&apos;m {dog.name}</h1>
       <img src={dog.img} alt={dog.name} />
       <div className="dog-info">
-      <p>Breed: {dog.breed}</p>
-      <p>Age: {dog.age}</p>
-      <p>Sex: {dog.sex}</p>
-      <p>Present: {dog.present ? "Yes" : "No"}</p>
-      <p>
-        Owner: {dog.owner.name} {dog.owner.lastName}
-      </p>
-      <p>Phone number: {dog.owner.phoneNumber}</p>
+      <p><strong>Breed:</strong> {dog.breed}</p>
+      <p>I&apos;m a {dog.age}-year-old {dog.sex.toLowerCase()} dog</p>
+      <p>I&apos;m currently {dog.present ? "here" : "not here right now"}</p>
+      <hr />
+      <p><PersonIcon className="icon-detail"/><strong>Owned by:</strong> {dog.owner.name} {dog.owner.lastName}</p>
+      <p><PhoneIcon className="icon-detail"/> <strong>Reach out at:</strong> {dog.owner.phoneNumber}</p>
       </div>
       </div>
     </div>
